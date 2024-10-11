771d2bcc7075e9a75e3d00d167b44d8868ee1829
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-10-10 20:40:27 -0400

    404 handling

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/master-of-none/rest-auth/routes"
 )
@@ -8,6 +10,12 @@ import (
 func main() {
 	r := gin.Default()
 
+	//! 404 Error Handler
+	r.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "404 not found",
+		})
+	})
 	routes.RegisterRoutes(r)
 
 	r.Run()
