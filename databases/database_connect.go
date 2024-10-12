9c319791d277ca434147741400a9f32801f2dd40
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-10-12 13:15:09 -0400

    Updated database code

--- databases/database_connect.go
+++ databases/database_connect.go
@@ -2,13 +2,11 @@ package databases
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -21,7 +19,7 @@ func ConnectDB(c *gin.Context) *mongo.Client {
 	opts := options.Client().ApplyURI(uri)
 
 	// Create a context with a timeout (optional but recommended)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	// Connecting client and connecting to the server
@@ -30,19 +28,6 @@ func ConnectDB(c *gin.Context) *mongo.Client {
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   "Failed to connect to the Database",
-			"details": err.Error(),
-		})
-	}
-
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			fmt.Printf("Error disconnecting from database: %v\n", err)
-		}
-	}()
-	var result bson.M
-	if err := client.Database("users").RunCommand(ctx, bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   "failed to ping the database",
 			"details": err.Error(),
 		})
 	}
